332254f478ef1c83ee0f38d5551991f258d85687
zhughes96 <35809394+zhughes96@users.noreply.github.com>
2020-04-10 03:44:45 -0400

    initial push

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+	Message Board Team
+	Parser Program
+	Zachary Hughes, Phil Iler, Charles Harover, Max Pelletier, Juntao Zhong
+	Purpose: takes in a string that can be read from IMAP and will
+	parse the information and display an output based on the different
+	key words in the string.
+	*/
+
+#include String
+
+int main(String info) {
+
+}
